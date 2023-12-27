6ffcf024b5dd1bb858dda46ac2fcba53f0b3b49f
jofan23 <jonathan.fan@yale.edu>
2023-12-27 14:14:49 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lonely',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='jofan1256',
+    author_email='jonathan.fan@yale.edu',
+    description='NLP Lonely Article Classification'
+)
