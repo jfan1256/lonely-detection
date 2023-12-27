5d782ceb19fb3108d87b948d993f38a216baedac
jofan23 <jonathan.fan@yale.edu>
2023-12-27 14:01:32 -0500

    first commit

---
+++ utils/system.py
@@ -0,0 +1,11 @@
+import os
+
+from pathlib import Path
+
+# Get root directory
+def get_root_dir() -> Path:
+    return Path(os.getcwd()).resolve()
+
+# Get data directory
+def get_data():
+    return get_root_dir() / 'data'
