f83c276e5a34de593bd32651590dba80c594b438
jofan23 <jonathan.fan@yale.edu>
2023-12-31 12:10:39 -0500

    editted system

--- utils/system.py
+++ utils/system.py
@@ -1,10 +1,8 @@
-import os
-
 from pathlib import Path
 
 # Get root directory
 def get_root_dir() -> Path:
-    return Path(os.getcwd()).resolve().parent
+    return Path(__file__).resolve().parent.parent
 
 # Get data directory
 def get_data():
